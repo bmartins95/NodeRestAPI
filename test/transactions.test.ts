213da5d659d83920be49df77a04961864ecf22de
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-06-27 17:19:57 -0300

    Add more tests

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,7 +1,9 @@
-import { afterAll, beforeAll, test, expect, describe } from 'vitest'
+import { afterAll, beforeAll, test, expect, describe, beforeEach } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+import { any } from 'zod'
 
 describe('Transactions routes', () => {
     beforeAll(
@@ -16,6 +18,11 @@ describe('Transactions routes', () => {
         }
     )
 
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
+    })
+
     test('Create new transaction returns 201', async () => {
         const response = await request(app.server)
             .post('/transactions')
@@ -26,6 +33,71 @@ describe('Transactions routes', () => {
             })
 
         expect(response.status).toBe(201)
+    })
+
+    test('List transactions should return 200 and match values', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 200,
+                type: 'credit',
+            })
+
+        const listResponse = await request(app.server).get('/transactions').expect(200)
+        expect(listResponse.body.transactions).toContainEqual(
+            {
+                title: 'New transaction',
+                amount: 200,
+                created_at: expect.any(String),
+                id: expect.any(String),
+                session_id: null,
+            },
+        )
+    })
+
+    test('Get transaction should return 200 and match values', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 200,
+                type: 'credit',
+            })
+
+        const listResponse = await request(app.server).get('/transactions')
+        const transactionId = listResponse.body.transactions[0].id
+        const getTransactionResponse = await request(app.server).get(`/transactions/${transactionId}`)
+        expect(getTransactionResponse.body.transaction).toEqual(
+            {
+                title: 'New transaction',
+                amount: 200,
+                created_at: expect.any(String),
+                id: expect.any(String),
+                session_id: null,
+            },
+        )
+    })
+
+    test('Get transaction summary should return 200 and match sum', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 600,
+                type: 'credit',
+            })
+
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 200,
+                type: 'debit',
+            })
+
+        const summaryResponse = await request(app.server).get('/transactions/summary')
+        expect(summaryResponse.body.summary.amount).toEqual(400)
     })
 })
 
