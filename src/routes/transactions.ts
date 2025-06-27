9c9ce822aa241f2151f5cb9555d617194799c55a
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-06-27 12:24:16 -0300

    Add more routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,15 +1,38 @@
+import { randomUUID } from "node:crypto"
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
-import { randomUUID } from "node:crypto"
 
 import { kx } from "../database"
+import knex from "knex"
 
 
 export async function transactions(app: FastifyInstance) {
     app.get(
         '/',
         async () => {
-            return await kx('transactions').select('*')
+            const transactions = await kx('transactions').select()
+            return { transactions }
+        }
+    )
+
+    app.get(
+        '/:id',
+        async (request) => {
+            const getTransactionSchema = z.object({
+                id: z.string().uuid(),
+            })
+            const { id } = getTransactionSchema.parse(request.params)
+
+            const transaction = await kx('transactions').where('id', id).first()
+            return { transaction }
+        }
+    )
+
+    app.get(
+        '/summary',
+        async () => {
+            const summary = await kx('transactions').sum('amount', { as: 'amount' }).first()
+            return { summary }
         }
     )
 
@@ -22,11 +45,13 @@ export async function transactions(app: FastifyInstance) {
                 type: z.enum(['credit', 'debit'])
             })
             const { title, amount, type } = createTransactionSchema.parse(request.body)
+
             await kx('transactions').insert({
                 id: randomUUID(),
                 title: title,
                 amount: type === 'credit' ? amount : amount * -1,
             })
+
             reply.code(201).send()
         }
     )
