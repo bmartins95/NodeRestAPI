9e5376446bbf33478f5d33826002fb9eec10c9bf
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-07-01 12:06:21 -0300

    Fix render no ports detected

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { app } from "./app"
 import { env } from "./env"
 
-app.listen({ port: env.PORT }).then(
-    () => {
-        console.log('Server running...')
-    }
-)
+app.listen({ port: env.PORT, host: "0.0.0.0" })
+    .then(address => {
+        console.log(`Server running at ${address}`)
+    })
+    .catch(err => {
+        app.log.error(err)
+        process.exit(1)
+    })
